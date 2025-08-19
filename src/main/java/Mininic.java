56ed9e696f58966b4d2714c678a194b602b555c9
nicholaslimzixian <pingboyniclim@gmail.com>
2025-08-19 11:56:07 +0800

    Add greeting

---
+++ src/main/java/Mininic.java
@@ -0,0 +1,12 @@
+public class Mininic {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hi! I'm Mininic");
+        System.out.println("Your wish is my command!");
+        System.out.println(LINE);
+        System.out.println("Bye :'(");
+        System.out.println(LINE);
+    }
+}
