a32ee0d361e7dbd1a17c118a28d4613cc955f570
nicholaslimzixian <pingboyniclim@gmail.com>
2025-08-19 12:18:59 +0800

    Add list

--- src/main/java/Mininic.java
+++ src/main/java/Mininic.java
@@ -14,14 +14,34 @@ public class Mininic {
     public static void main(String[] args) {
         box("Hello! I'm Mininic", "Your wish is my command!");
 
+        String[] tasks = new String[100];
+        int size = 0;
+
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            if ("bye".equals(input.trim())) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
                 box("Bye... :'(");
+                sc.close();
                 break;
+            } else if (input.equals("list")) {
+                if (size == 0) {
+                    box();
+                } else {
+                    String[] lines = new String[size];
+                    for (int i = 0; i < size; i++) {
+                        lines[i] = (i + 1) + ". " + tasks[i];
+                    }
+                    box(lines);
+                }
+            } else {
+                if (size < tasks.length) {
+                    tasks[size++] = input;
+                    box("Task added: " + input);
+                } else {
+                    box("I'm very full... Clear some tasks to add more!");
+                }
             }
-            box(input);
         }
     }
 }
